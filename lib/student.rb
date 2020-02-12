987ced24c015215c3abe0a21805cfa1c507c83d2
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-12 21:03:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,13 +91,12 @@ class Student
       end
   end
 
-  def self.first_X_students_in_grade_10(x)
-    x = x
+  def self.first_X_students_in_grade_10(arg)
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT x
+      LIMIT arg
          SQL
 
       DB[:conn].execute(sql).map do |row|
